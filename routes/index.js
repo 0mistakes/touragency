8bdadc1222ab8a7a83988ad0cdc9e43c460527e0
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2022-11-16 16:31:51 +0300

    commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница глобуса */
+router.get('/globus', function(req, res, next) {
+  res.send("<h1>Страница globus</h1>")
+});
+
+/* Страница kartinka*/
+router.get('/kartinka', function(req, res, next) {
+  res.send("<h1>Страница kartinka</h1>")
+});
+
+/* Страница chto-to eshe*/
+router.get('/generic', function(req, res, next) {
+  res.send("<h1>Страница generic</h1>")
+});
+
 module.exports = router;
