65b3e0b14a93d700289159bc633c746ecc47c2aa
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2022-11-23 16:51:51 +0300

    nodeejs

---
+++ createDB.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
+
+const uri = "mongodb://127.0.0.1:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try {
+        await client.connect();
+        var database = client.db("agency");
+        database.dropDatabase()
+        database = client.db("agency");
+        const agencys = database.collection("travel");
+        const result = await agencys.insertOne({name:"Standart"});
+        console.log(`${result} documents were inserted`);
+    } finally {
+        await client.close();
+    }
+}
+run()
---
+++ data.js
@@ -0,0 +1,22 @@
+var data = [{
+    title: 'Standart',
+    nick: 'standart',
+    avatar: '/images/travel-agency.png',
+    desc: 'Standart agency'
+    },
+    {
+    title: 'Kartinka',
+    nick: 'kartinka',
+    avatar: '/images/photo-kartinka.png',
+    desc: "kartinka agency"
+    },
+    {
+    title: 'Globus',
+    nick: 'globus',
+    avatar: '/images/photo-globus',
+    desc: 'globus photo'
+    }
+];
+
+
+module.exports.data = data;
