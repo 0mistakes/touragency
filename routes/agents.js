cf33399cde0f0c0a9c7dc6a003f36b3413d0c84f
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2022-11-30 20:18:19 +0300

    br_8_3

--- routes/agents.js
+++ routes/agents.js
@@ -10,5 +10,17 @@ router.get("/:nick", function(req, res, next) {
   res.send(req.params.nick);
 });
 
+router.get('/:nick', function(req, res, next) {
+  Cat.findOne({nick:req.params.nick}, function(err,agent){
+      if(err) return next(err)
+      if(!agent) return next(new Error("Нет такой продукции в нашем агентстве"))
+      res.render('cat', {
+          title: agent.title,
+          picture: agent.avatar,
+          desc: agent.desc
+      })
+  })
+})
+
 
 module.exports = router;
