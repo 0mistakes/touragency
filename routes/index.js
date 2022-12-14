aed5e412544ca7ab39fa5488dcfaafc174be6456
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2022-12-14 21:44:35 +0300

    9.5

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,13 @@ var Agent = require("../models/agent").Agent
 
 router.get('/', function(req, res, next) {
     Agent.find({},{_id:0,title:1,nick:1},function(err,menu){
+      req.session.greeting = "Session started"
         res.render('index', {
-                                title: 'Express',
-                                menu: menu
+          title: 'Touragency',
+          counter: req.session.counter
                             });
     })
 
 });
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Express',
-    menu:menu,
-    counter:req.session.counter 
-});
-});
-
 
 module.exports = router;
