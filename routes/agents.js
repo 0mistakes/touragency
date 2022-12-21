501180096f2e8ab0f12ce8355eed222bc23d921a
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2022-12-21 21:44:34 +0300

    10.8

--- routes/agents.js
+++ routes/agents.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Agent = require('../models/agent').Agent;
+var checkAuth = require("./../middleware/checkAuth.js");
 //var async = require("async");
 
 
@@ -9,7 +10,7 @@ router.get('/', function(req, res, next) {
     res.send('Новый маршрутизатор для маршрутов начинающихся с agents')
 })
 
-router.get('/:nick', function(req, res, next) {
+router.get('/:nick',checkAuth, function(req, res, next) {
     Agent.findOne({nick: req.params.nick}, function(err, agent) {
         if(err) return next(err)
         if(!agent) return next(new Error("There`s no such page. Try again."))
